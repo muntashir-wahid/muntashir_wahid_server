ad295831471758bf3aa868402b6c3ac8729b966e
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-01-12 23:53:47 +0600

    working on code refactoring

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const morgan = require("morgan");
-require("dotenv").config();
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
